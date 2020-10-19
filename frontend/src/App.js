46af50102bad775671c102f92669c655b1a34ea3
Rahi <rahidul_islam@criptext.com>
2020-10-19 08:52:08 +0600

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import TopNavigation from './Components/TopNavigation/TopNavigation';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './Router/AppRouter';
+
 
 function App() {
   return (
-    <div className="App">
-      <TopNavigation/>
-    </div>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import TopNavigation from '../Components/TopNavigation/TopNavigation';
+class HomePage extends Component {
+    render() {
+        return (
+            <Fragment>
+                <TopNavigation/>
+            </Fragment>
+        );
+    }
+}
+
+export default HomePage;
