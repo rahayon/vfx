c152ee0c4a14287aa7b599957ea3ed9740537c20
Rahi <rahidul_islam@criptext.com>
2020-10-19 22:07:15 +0600

    Summery
    summery complete. only css will be written

---
+++ frontend/src/Components/Summery/Summery.jsx
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+class Summery extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <div>
+                        <Row>
+                            <Col>
+                            <h1>
+                                <CountUp start={0} end={500}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <span ref={countUpRef} />
+                                      </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </h1>
+                            </Col>
+                        </Row>
+                    </div>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default Summery;
